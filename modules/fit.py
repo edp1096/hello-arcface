1958c2a7ccc2b341e83a701840c9401703313b5a
edp1096 <edp1096@naver.com>
2023-02-26 08:02:10 +0900

    변수명 바꿈

--- modules/fit.py
+++ modules/fit.py
@@ -3,12 +3,12 @@ def runCNN(device, loader, model, loss_fn, optimizer):
 
     size = len(loader.dataset)
 
-    for batch, (X, y) in enumerate(loader):
-        X, y = X.to(device), y.to(device)
+    for batch, (image, label) in enumerate(loader):
+        image, label = image.to(device), label.to(device)
 
         # 예측 오류 계산
-        pred = model(X.float())
-        loss = loss_fn(pred, y)
+        pred = model(image.float())
+        loss = loss_fn(pred, label)
 
         # 역전파
         optimizer.zero_grad()
@@ -16,5 +16,5 @@ def runCNN(device, loader, model, loss_fn, optimizer):
         optimizer.step()
 
         if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
+            loss, current = loss.item(), batch * len(image)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
