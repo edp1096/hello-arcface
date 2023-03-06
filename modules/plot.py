2d194ee2f0f0403161da7a280f5dbf2890571aaf
edp1096 <edp1096@naver.com>
2023-03-07 06:52:19 +0900

    필요없는 주석 제거

--- modules/plot.py
+++ modules/plot.py
@@ -27,13 +27,10 @@ def plotTrainResults(train_accs, valid_accs, train_losses, valid_losses, best_ep
     plt.show()
 
 
-# Define our own plot function
 def scatter(x, labels, root=".", subtitle=None, dataset="MNIST"):
+    num_classes = len(set(labels))
+    palette = np.array(sns.color_palette("hls", num_classes))
 
-    num_classes = len(set(labels))  # Calculate the number of classes
-    palette = np.array(sns.color_palette("hls", num_classes))  # Choosing color
-
-    ## Create a seaborn scatter plot ##
     f = plt.figure(figsize=(8, 8))
     ax = plt.subplot(aspect="equal")
     sc = ax.scatter(x[:, 0], x[:, 1], lw=0, s=40, c=palette[labels.astype(np.int)])
@@ -41,9 +38,7 @@ def scatter(x, labels, root=".", subtitle=None, dataset="MNIST"):
     plt.ylim(-25, 25)
     ax.axis("off")
     ax.axis("tight")
-    ## ---------------------------- ##
 
-    ## Add label on top of each cluster ##
     if dataset == "MNIST":
         idx2name = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     elif dataset == "CIFAR10" or dataset == "STL10":
@@ -58,17 +53,12 @@ def scatter(x, labels, root=".", subtitle=None, dataset="MNIST"):
         if len(idx2name) == 0:
             raise Exception("Please specify the dataset")
 
-        # raise Exception("Please specify the dataset")
-
     txts = []
     for i in range(num_classes):
-        # Position of each label.
         xtext, ytext = np.median(x[labels == i, :], axis=0)
         txt = ax.text(xtext, ytext, idx2name[i], fontsize=24)
         txt.set_path_effects([PathEffects.Stroke(linewidth=5, foreground="w"), PathEffects.Normal()])
         txts.append(txt)
-
-    ## ---------------------------- ##
 
     if subtitle != None:
         plt.suptitle(subtitle)
