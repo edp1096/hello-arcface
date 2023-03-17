c088383908460c76dd709250af10056e6f1a44c0
edp1096 <edp1096@naver.com>
2023-03-17 10:35:52 +0900

    irsnet 안해

--- modules/net.py
+++ modules/net.py
@@ -5,9 +5,6 @@ import torch.nn as nn
 from torchvision import datasets, models, transforms
 import timm
 
-# from facenet_pytorch import MTCNN, InceptionResnetV1
-from modules.inception_resnet_v1 import InceptionResnetV1
-
 from modules.loss import ArcFace
 
 
@@ -26,8 +23,8 @@ class NetHead(nn.Module):
                     weights = models.ResNet18_Weights.IMAGENET1K_V1
                 self.model = models.resnet18(weights=weights)
                 in_features, self.model.fc = self.model.fc.in_features, nn.Identity()
-                for param in self.model.parameters():
-                    param.requires_grad = False
+                # for param in self.model.parameters():
+                #     param.requires_grad = False
                 # self.model.avgpool.register_forward_hook(self.getEmbeddingHook)
             case "efficientnetv2_s":
                 self.model = timm.create_model(
@@ -38,8 +35,8 @@ class NetHead(nn.Module):
                     drop_path_rate=0.2,
                 )
                 in_features, self.model.classifier = self.model.classifier.in_features, nn.Identity()
-                for param in self.model.parameters():
-                    param.requires_grad = False
+                # for param in self.model.parameters():
+                #     param.requires_grad = False
                 # self.model.global_pool.register_forward_hook(self.getEmbeddingHook)
 
         self.fc = nn.Linear(in_features, num_classes)
