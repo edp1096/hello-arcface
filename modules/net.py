14d41a5931fec76d96c359f5712466f6049cbd39
edp1096 <edp1096@naver.com>
2023-03-23 21:20:23 +0900

    3.10 안해

--- modules/net.py
+++ modules/net.py
@@ -17,23 +17,17 @@ class NetHead(nn.Module):
 
         self.embedding = None  # features embedding
 
-        match MODEL_NAME:
-            case "resnet18":
-                if pretrained:
-                    weights = models.ResNet18_Weights.IMAGENET1K_V1
-                self.model = models.resnet18(weights=weights)
-                in_features, self.model.fc = self.model.fc.in_features, nn.Identity()
-                # self.model.avgpool.register_forward_hook(self.getEmbeddingHook)
-            case "efficientnetv2_s":
-                self.model = timm.create_model(
-                    "tf_efficientnetv2_s_in21k",
-                    num_classes=num_classes,
-                    pretrained=pretrained,
-                    drop_rate=0.2,
-                    drop_path_rate=0.2,
-                )
-                in_features, self.model.classifier = self.model.classifier.in_features, nn.Identity()
-                # self.model.global_pool.register_forward_hook(self.getEmbeddingHook)
+        if MODEL_NAME == "resnet18":
+            if pretrained:
+                weights = models.ResNet18_Weights.IMAGENET1K_V1
+            self.model = models.resnet18(weights=weights)
+            in_features, self.model.fc = self.model.fc.in_features, nn.Identity()
+            # self.model.avgpool.register_forward_hook(self.getEmbeddingHook)
+        elif MODEL_NAME == "efficientnetv2_s":
+            model_name = "tf_efficientnetv2_s_in21k"
+            self.model = timm.create_model(model_name, num_classes=num_classes, pretrained=pretrained, drop_rate=0.2, drop_path_rate=0.2)
+            in_features, self.model.classifier = self.model.classifier.in_features, nn.Identity()
+            # self.model.global_pool.register_forward_hook(self.getEmbeddingHook)
 
         self.fc = nn.Linear(in_features, num_classes)
         self.arcface = ArcFace(num_classes, num_classes, s=30.0, m_arc=0.5)
